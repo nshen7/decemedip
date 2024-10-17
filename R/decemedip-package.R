b9fe10a57f754fd3add9f1c0efea98b2e0f6946c
nshen7 <shenning1997@gmail.com>
2024-10-17 16:00:49 -0700

    change package file alias

--- R/decemedip-package.R
+++ R/decemedip-package.R
@@ -14,5 +14,5 @@
 #' @references
 #' Stan Development Team (NA). RStan: the R interface to Stan. R package version 2.32.6. https://mc-stan.org
 #'
-#' @aliases decemedip
+#' @aliases decemedip-package
 "_PACKAGE"
