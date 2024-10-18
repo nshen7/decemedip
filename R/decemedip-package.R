3edd9356722fd1b997aa1f193bdf5c3678ceeb34
nshen7 <shenning1997@gmail.com>
2024-10-17 17:19:54 -0700

    change package file alias

--- R/decemedip-package.R
+++ R/decemedip-package.R
@@ -1,6 +1,10 @@
 #' The 'decemedip' package.
 #'
-#' @description A DESCRIPTION OF THE PACKAGE
+#' @description The R package decemedip is a novel computational paradigm developed
+#' for inferring the relative abundances of cell types and tissues from tissue bulk
+#' or circulating cell-free DNA (cfDNA) measure by methylated DNA immunoprecipitation
+#' sequencing (MeDIP-Seq). This paradigm allows using reference data from other
+#' technologies such as microarray or WGBS.
 #'
 #' @docType package
 #' @name decemedip-package
